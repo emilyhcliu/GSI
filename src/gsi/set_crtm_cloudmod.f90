dc27fd8d83f84038fdde53c49ee086916540b587
Emily Liu <emily.liu@noaa.gov>
2024-05-20 16:15:28 +0000

    Add handling for using Thompson physics based on feature/ThompsonMP_Azadeh

--- src/gsi/set_crtm_cloudmod.f90
+++ src/gsi/set_crtm_cloudmod.f90
@@ -168,6 +168,7 @@ CONTAINS
        ! --------------------------------------------------
 !    Loop over clouds ...
 !    --------------------
+
      do n = 1, nc
 
 !       Map Model cloud names into CRTM Cloud indices
@@ -187,7 +188,7 @@ CONTAINS
               cloud(n)%Effective_Radius(:) = cloud_efr(:,n)
            else
              !cloud(n)%Effective_Radius(:) = EftSize_(cloud_name(jcloud(n))) 
-              if ( imp_physics==11 .and. lprecip ) then
+              if (lprecip ) then
                  cloud(n)%Effective_Radius(:) = cloud_efr(:,n)
               else
                  do k = 1, km
